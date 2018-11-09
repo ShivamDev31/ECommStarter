178bed84ac5a5b576dd239dfda601a67ab403997
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-09 21:28:50 +0530

    Fetch and log data

--- app/src/main/java/io/kotlin/ecommstarter/home/HomeActivity.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/HomeActivity.kt
@@ -1,13 +1,26 @@
 package io.kotlin.ecommstarter.home
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import dagger.android.AndroidInjection
 import io.kotlin.ecommstarter.R
+import kotlinx.android.synthetic.main.activity_feed.*
+import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity() {
 
+    @Inject lateinit var homePresenter: HomePresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
         setContentView(R.layout.activity_feed)
+        homePresenter.startPresenting()
+    }
+
+    override fun onStop() {
+        homePresenter.stopPresenting()
+        super.onStop()
     }
 }
