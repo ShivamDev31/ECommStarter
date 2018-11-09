ddbef3ec604ce5e56770d596afb8175ca266dc96
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-09 21:18:57 +0530

    Updated core to work with Json adapter

--- core/src/main/java/io/kotlin/ecommstarter/home/api/ApiProductBlock.kt
+++ core/src/main/java/io/kotlin/ecommstarter/home/api/ApiProductBlock.kt
@@ -3,6 +3,7 @@ package io.kotlin.ecommstarter.home.api
 import io.kotlin.ecommstarter.home.Block
 
 data class ApiProductBlock(val type: String, val products: List<ApiProducts>?) : ApiBlock {
+
     override fun accept(visitor: ApiBlock.Visitor): Block {
         return visitor.visit(this)
     }
