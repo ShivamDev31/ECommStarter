5aa31c7755afe9b668269cea8ee96f04dc1c26ef
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-12 14:17:18 +0530

    Refactor the activity_main to activity_home

--- app/src/main/java/io/kotlin/ecommstarter/home/HomeActivity.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/HomeActivity.kt
@@ -2,10 +2,8 @@ package io.kotlin.ecommstarter.home
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import dagger.android.AndroidInjection
 import io.kotlin.ecommstarter.R
-import kotlinx.android.synthetic.main.activity_feed.*
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity() {
@@ -15,7 +13,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
-        setContentView(R.layout.activity_feed)
+        setContentView(R.layout.activity_home)
         homePresenter.startPresenting()
     }
 
