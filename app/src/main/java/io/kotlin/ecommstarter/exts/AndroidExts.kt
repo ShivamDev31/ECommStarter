4bb48397bc83f65757bd7bfb37dcf7ca7c15ec4d
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 18:44:55 +0530

    Add methods to show and hide view in exts

--- app/src/main/java/io/kotlin/ecommstarter/exts/AndroidExts.kt
+++ app/src/main/java/io/kotlin/ecommstarter/exts/AndroidExts.kt
@@ -1,6 +1,9 @@
 package io.kotlin.ecommstarter.exts
 
 import android.content.Context
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import androidx.annotation.StringRes
 
@@ -10,4 +13,12 @@ fun Context.toast(text: String) {
 
 fun Context.toast(@StringRes text: Int) {
     Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+}
+
+fun View.hide() {
+    visibility = GONE
+}
+
+fun View.show() {
+    visibility = VISIBLE
 }
