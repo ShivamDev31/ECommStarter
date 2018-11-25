58e46e7f82242b239e78df6f61b68962b5affb86
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 11:30:49 +0530

    Add view types in ViewState

--- app/src/main/java/io/kotlin/ecommstarter/home/viewstate/HomeViewState.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/viewstate/HomeViewState.kt
@@ -7,7 +7,9 @@ interface HomeViewState {
 
     enum class Type constructor(@LayoutRes val layoutId: Int) {
         SLIDER_PRODUCT(R.layout.item_product_slider),
-        PRODUCTS_LIST(R.layout.item_product_list);
+        LIST_PRODUCT(R.layout.item_product_list),
+        GRID_PRODUCT(R.layout.item_product_grid),
+        AD_BLOCK(R.layout.item_ad_block);
 
         companion object {
             fun from(value: Int): Type = Type.values()[value]
