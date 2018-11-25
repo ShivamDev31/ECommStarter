c88c0fcc105f2a62b1d67444f951c8734f6aacdb
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 18:47:12 +0530

    Add required modules in dagger graph

--- app/src/main/java/io/kotlin/ecommstarter/app/EcommerceApp.kt
+++ app/src/main/java/io/kotlin/ecommstarter/app/EcommerceApp.kt
@@ -1,10 +1,11 @@
 package io.kotlin.ecommstarter.app
 
 import android.app.Application
-import android.util.Log
 import io.kotlin.ecommstarter.BuildConfig
 import io.kotlin.ecommstarter.di.ApplicationInjector
+import io.kotlin.ecommstarter.di.ApplicationModule
 import io.kotlin.ecommstarter.di.DaggerApplicationInjector
+import io.kotlin.ecommstarter.imageloader.ImageLoaderModule
 import io.kotlin.ecommstarter.network.JsonModule
 import io.kotlin.ecommstarter.network.NetworkModule
 import timber.log.Timber
@@ -16,8 +17,10 @@ class EcommerceApp : InjectableApp(ApplicationAndroidFactory()) {
     class ApplicationAndroidFactory : ApplicationInjector.Factory {
         override fun create(application: Application): ApplicationInjector {
             return DaggerApplicationInjector.builder()
+                    .applicationModule(ApplicationModule(application))
                     .networkModule(NetworkModule(application))
                     .jsonModule(JsonModule())
+                    .imageLoaderModule(ImageLoaderModule())
                     .build()
         }
     }
--- app/src/main/java/io/kotlin/ecommstarter/di/ApplicationModule.kt
+++ app/src/main/java/io/kotlin/ecommstarter/di/ApplicationModule.kt
@@ -4,11 +4,12 @@ import android.app.Application
 import dagger.Module
 import dagger.Provides
 import io.kotlin.ecommstarter.common.AndroidAssetLoader
+import io.kotlin.ecommstarter.imageloader.ImageLoaderModule
 import io.kotlin.ecommstarter.network.JsonModule
 import io.kotlin.ecommstarter.network.NetworkModule
 import javax.inject.Singleton
 
-@Module(includes = [NetworkModule::class, JsonModule::class])
+@Module(includes = [NetworkModule::class, JsonModule::class, ImageLoaderModule::class])
 class ApplicationModule(private val application: Application) {
 
     @Provides
