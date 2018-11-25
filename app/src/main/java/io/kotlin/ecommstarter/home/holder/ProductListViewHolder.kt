3e5fa15e3fc606024ff6cb91b7dc9f076348dd2c
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 10:37:27 +0530

    Create Product List and Slider ViewHolder

---
+++ app/src/main/java/io/kotlin/ecommstarter/home/holder/ProductListViewHolder.kt
@@ -0,0 +1,13 @@
+package io.kotlin.ecommstarter.home.holder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import io.kotlin.ecommstarter.home.viewstate.HomeViewState
+
+class ProductListViewHolder(inflater: LayoutInflater, parent: ViewGroup) : HomeViewHolder(inflater, parent, HomeViewState.Type.PRODUCTS_LIST) {
+
+    override fun bind(viewState: HomeViewState) {
+        
+    }
+
+}
---
+++ app/src/main/java/io/kotlin/ecommstarter/home/holder/ProductSliderViewHolder.kt
@@ -0,0 +1,12 @@
+package io.kotlin.ecommstarter.home.holder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import io.kotlin.ecommstarter.home.viewstate.HomeViewState
+
+class ProductSliderViewHolder(inflater: LayoutInflater, parent: ViewGroup): HomeViewHolder(inflater, parent, HomeViewState.Type.SLIDER_PRODUCT) {
+
+    override fun bind(viewState: HomeViewState) {
+
+    }
+}
