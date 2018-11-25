89472a6367cca6f5e3007e3633de9792ec5a7be7
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 12:42:47 +0530

    Update HomeUseCase to handle loading and error states

--- app/src/main/java/io/kotlin/ecommstarter/home/HomeActivityModule.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/HomeActivityModule.kt
@@ -5,11 +5,20 @@ import dagger.Module
 import dagger.Provides
 import io.kotlin.ecommstarter.home.api.HomeApiFetcher
 import io.kotlin.ecommstarter.home.api.HomeFetcher
+import io.kotlin.ecommstarter.home.viewstate.HomeViewStateConverter
+import io.kotlin.ecommstarter.imageloader.ImageLoader
+import io.kotlin.ecommstarter.navigator.AndroidNavigator
+import io.kotlin.ecommstarter.navigator.Navigator
 import io.kotlin.ecommstarter.rx.AndroidSchedulingStrategyFactory
 import retrofit2.Retrofit
 
 @Module
 class HomeActivityModule {
+
+    @Provides
+    fun navigator(activity: HomeActivity): Navigator {
+        return AndroidNavigator(activity)
+    }
 
     @Provides
     fun ecommerceFetcher(retrofit: Retrofit, moshi: Moshi): HomeFetcher {
@@ -18,12 +27,15 @@ class HomeActivityModule {
 
     @Provides
     fun homeUseCase(homeFetcher: HomeFetcher): HomeUseCase {
-        return HomeUseCase(homeFetcher, AndroidSchedulingStrategyFactory.io())
+        val converter = HomeViewStateConverter()
+        return HomeUseCase(homeFetcher, converter, AndroidSchedulingStrategyFactory.io())
     }
 
     @Provides
-    fun homePresenter(homeUseCase: HomeUseCase): HomePresenter {
-        return HomePresenter(homeUseCase)
+    fun homePresenter(homeUseCase: HomeUseCase, activity: HomeActivity, imageLoader: ImageLoader, navigator: Navigator): HomePresenter {
+        val homeView = HomeView.from(activity, imageLoader)
+        val homeDisplayer = HomeDisplayer(homeView)
+        return HomePresenter.create(homeUseCase, homeDisplayer, navigator)
     }
 
 }
--- app/src/main/java/io/kotlin/ecommstarter/home/HomeUseCase.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/HomeUseCase.kt
@@ -1,17 +1,25 @@
 package io.kotlin.ecommstarter.home
 
 import io.kotlin.ecommstarter.home.api.HomeFetcher
+import io.kotlin.ecommstarter.home.viewstate.EcommerceHomeViewState
+import io.kotlin.ecommstarter.home.viewstate.HomeErrorViewStateConverter
+import io.kotlin.ecommstarter.home.viewstate.HomeViewStateConverter
+import io.kotlin.ecommstarter.home.viewstate.HomeViewStateConverter.HomeLoadingViewState
 import io.kotlin.ecommstarter.rx.AndroidSchedulingStrategyFactory
 import io.reactivex.Observable
 
 class HomeUseCase(private val homeFetcher: HomeFetcher,
+                  private val converter: HomeViewStateConverter,
                   private val schedulingStrategyFactory: AndroidSchedulingStrategyFactory) {
 
-    fun homeFeed(): Observable<HomeBlock> {
-        return homeFetcher.load()
-                .compose(schedulingStrategyFactory.create())
+    fun homeFeed(): Observable<EcommerceHomeViewState> {
+        val loadingViewState = HomeLoadingViewState()
+        return homeFetcher
+                .load()
                 .toObservable()
-
+                .map(converter)
+                .startWith(loadingViewState)
+                .compose(HomeErrorViewStateConverter())
+                .compose(schedulingStrategyFactory.create())
     }
-
 }
