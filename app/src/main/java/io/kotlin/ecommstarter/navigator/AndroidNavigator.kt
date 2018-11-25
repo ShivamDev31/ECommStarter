5368a3ed250ce5191ef509212fe2e6591638898b
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 12:24:05 +0530

    Create Navigator for application navigation

---
+++ app/src/main/java/io/kotlin/ecommstarter/navigator/AndroidNavigator.kt
@@ -0,0 +1,11 @@
+package io.kotlin.ecommstarter.navigator
+
+import android.app.Activity
+import android.content.Intent
+
+class AndroidNavigator(private val activity: Activity) : Navigator {
+
+    private fun startActivity(intent: Intent) {
+        activity.startActivity(intent)
+    }
+}
---
+++ app/src/main/java/io/kotlin/ecommstarter/navigator/Navigator.kt
@@ -0,0 +1,5 @@
+package io.kotlin.ecommstarter.navigator
+
+interface Navigator {
+
+}
