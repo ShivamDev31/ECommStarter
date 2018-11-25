18d5db22e48ce369904f4db40d30f65139212f6c
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 11:29:32 +0530

    Create different visitors in Block

--- core/src/main/java/io/kotlin/ecommstarter/home/GridProductBlock.kt
+++ core/src/main/java/io/kotlin/ecommstarter/home/GridProductBlock.kt
@@ -3,6 +3,6 @@ package io.kotlin.ecommstarter.home
 data class GridProductBlock(val product: Product) : Block {
 
     override fun accept(visitor: Block.Visitor) {
-        visitor.visit(this.product)
+        visitor.visit(this)
     }
 }
--- core/src/main/java/io/kotlin/ecommstarter/home/ListProductBlock.kt
+++ core/src/main/java/io/kotlin/ecommstarter/home/ListProductBlock.kt
@@ -3,6 +3,6 @@ package io.kotlin.ecommstarter.home
 data class ListProductBlock(val product: Product) : Block {
 
     override fun accept(visitor: Block.Visitor) {
-        visitor.visit(this.product)
+        visitor.visit(this)
     }
 }
