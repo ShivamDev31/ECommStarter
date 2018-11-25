da91b684a2b0a5ef36e62da8b4dafcd5447b4880
Shivam Chopra <shivam.chopra@news.co.uk>
2018-11-25 18:44:23 +0530

    Add local and remote in module

--- app/src/main/java/io/kotlin/ecommstarter/home/HomeActivityModule.kt
+++ app/src/main/java/io/kotlin/ecommstarter/home/HomeActivityModule.kt
@@ -3,14 +3,17 @@ package io.kotlin.ecommstarter.home
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import io.kotlin.ecommstarter.common.AssetLoader
 import io.kotlin.ecommstarter.home.api.HomeApiFetcher
 import io.kotlin.ecommstarter.home.api.HomeFetcher
+import io.kotlin.ecommstarter.home.api.HomeLocalFetcher
 import io.kotlin.ecommstarter.home.viewstate.HomeViewStateConverter
 import io.kotlin.ecommstarter.imageloader.ImageLoader
 import io.kotlin.ecommstarter.navigator.AndroidNavigator
 import io.kotlin.ecommstarter.navigator.Navigator
 import io.kotlin.ecommstarter.rx.AndroidSchedulingStrategyFactory
 import retrofit2.Retrofit
+import javax.inject.Named
 
 @Module
 class HomeActivityModule {
@@ -21,12 +24,19 @@ class HomeActivityModule {
     }
 
     @Provides
-    fun ecommerceFetcher(retrofit: Retrofit, moshi: Moshi): HomeFetcher {
+    @Named("remote")
+    fun ecommerceApiFetcher(retrofit: Retrofit, moshi: Moshi): HomeFetcher {
         return HomeApiFetcher.from(retrofit, moshi)
     }
 
     @Provides
-    fun homeUseCase(homeFetcher: HomeFetcher): HomeUseCase {
+    @Named("local")
+    fun ecommerceLocalFetcher(moshi: Moshi, assetLoader: AssetLoader): HomeFetcher {
+        return HomeLocalFetcher.from(moshi, assetLoader)
+    }
+
+    @Provides
+    fun homeUseCase(@Named("remote") homeFetcher: HomeFetcher): HomeUseCase {
         val converter = HomeViewStateConverter()
         return HomeUseCase(homeFetcher, converter, AndroidSchedulingStrategyFactory.io())
     }
